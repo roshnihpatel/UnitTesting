ccde07019042b228fb04826fc5bfa204e4d3a17a
Roshni <roshnihpatel@outlook.com>
2022-08-01 16:57:16 +0100

    changed Classification_Test test suite, all tests passing

--- src/test/java/com/sparta/rp/Classification_Tests.java
+++ src/test/java/com/sparta/rp/Classification_Tests.java
@@ -7,19 +7,19 @@ import org.junit.jupiter.api.Test;
 public class Classification_Tests {
     @Test
     @DisplayName("Given age is 4 return U, PG and 12 films are available")
-    void GivenAgeIs4ReturnUPG12FilmsAreAvailable(){
+    void GivenAgeIs4ReturnUAndPGFilmsAreAvailable(){
         int age = 4;
-        String expected = "U, PG & 12 films are available.";
-        String answer = FilmClassification.availableClassifications(4);
+        String expected = "U & PG films are available.";
+        String answer = FilmClassification.availableClassifications(age);
         Assertions.assertEquals(expected,answer);
 
     }
     @Test
     @DisplayName("Given age is 13 return U, PG , 12 and 15 films are available")
-    void GivenAgeIs13ReturnUPG12And15FilmsAreAvailable(){
-        int age = 16;
-        String expected = "U, PG, 12 & 15 films are available.";
-        String answer = FilmClassification.availableClassifications(13);
+    void GivenAgeIs13ReturnUPGAnd12FilmsAreAvailable(){
+        int age = 13;
+        String expected = "U, PG & 12 films are available.";
+        String answer = FilmClassification.availableClassifications(age);
         Assertions.assertEquals(expected,answer);
 
     }
@@ -27,8 +27,17 @@ public class Classification_Tests {
     @DisplayName("Given age is 35 all films are available.")
     void GivenAgeIs35ReturnAllFilmsAreAvailable(){
         int age = 35;
-        String expected = "All films are available.";
-        String answer = FilmClassification.availableClassifications(35);
+        String expected = "All films are available";
+        String answer = FilmClassification.availableClassifications(age);
+        Assertions.assertEquals(expected,answer);
+
+    }
+    @Test
+    @DisplayName("Given age is 16 return U, PG , 12 and 15 films are available")
+    void GivenAgeIs16ReturnUPG12And15FilmsAreAvailable(){
+        int age = 16;
+        String expected = "U, PG, 12 & 15 films are available.";
+        String answer = FilmClassification.availableClassifications(age);
         Assertions.assertEquals(expected,answer);
 
     }
