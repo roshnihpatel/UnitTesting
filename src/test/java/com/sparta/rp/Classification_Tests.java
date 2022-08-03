309087ed3fac80791259eb531c304167c6232a25
Roshni <roshnihpatel@outlook.com>
2022-08-03 16:43:36 +0100

    added parameterized tests for FilmClassification

--- src/test/java/com/sparta/rp/Classification_Tests.java
+++ src/test/java/com/sparta/rp/Classification_Tests.java
@@ -3,6 +3,8 @@ package com.sparta.rp;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class Classification_Tests {
     @Test
@@ -41,5 +43,39 @@ public class Classification_Tests {
         Assertions.assertEquals(expected,answer);
 
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,4,5})
+    @DisplayName("Check for multiple value in a test")
+    void checkForMultipleValuesInTest(int number){
+        Assertions.assertEquals(true,number<6);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,4,5,7,9,10,11})
+    @DisplayName("Given ages are under 12 return U and PG films are available")
+    void givenAgesAreUnder12ReturnUAndPgFilmsAreAvailable(int age){
+        String expected = "U & PG films are available.";
+        String answer = FilmClassification.availableClassifications(age);
+        Assertions.assertEquals(expected,answer);
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {12,13,14})
+    @DisplayName("Given ages are between 12 and 14 return U, PG and 12 films are available")
+    void givenAgesAreBetween12And14ReturnUPgAnd12FilmsAreAvailable(int age){
+        String expected = "U, PG & 12 films are available.";
+        String answer = FilmClassification.availableClassifications(age);
+        Assertions.assertEquals(expected,answer);
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {15,16,17})
+    @DisplayName("Given ages are between 15 and 17 return U, PG, 12 and 15 films are available")
+    void givenAgesAreBetween15And17ReturnUPg12And15FilmsAreAvailable(int age){
+        String expected = "U, PG, 12 & 15 films are available.";
+        String answer = FilmClassification.availableClassifications(age);
+        Assertions.assertEquals(expected,answer);
+    }
+
+
 
 }
